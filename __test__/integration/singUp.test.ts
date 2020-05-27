b425f39521ec7c6d41f86c1419308ee2bffa9c53
Luis Felipe Cavalari <lfelipe.felipe4@gmail.com>
2020-05-27 00:28:19 -0300

    feat: connect mongodb before server starts

--- __test__/integration/singUp.test.ts
+++ __test__/integration/singUp.test.ts
@@ -1,7 +1,20 @@
 import app from '../../src/main/config/app'
 import request from 'supertest'
+import { mongoHelper } from '../../src/infra/database/mongodb/helper/mongoDbHelper'
 
 describe('SignUp route', () => {
+  beforeAll(async () => {
+    await mongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await mongoHelper.desconect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = mongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return an account ', async () => {
     await request(app).post('/api/signup').send({
       name: 'Luis Cavalari',
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,9 @@
 import app from './config/app'
+import env from './config/env'
+import { mongoHelper } from '../infra/database/mongodb/helper/mongoDbHelper'
 
-app.listen(3333, () => {
-  console.log('Server runing at http://localhost:3333')
-})
+mongoHelper.connect(env.MONGO_URL).then(() => {
+  app.listen(env.PORT, () => {
+    console.log(`Server runing at http://localhost:${env.PORT}`)
+  })
+}).catch(console.error)
